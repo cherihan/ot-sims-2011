914e5b0900edd4ceba23abb683654300d01ea16c
studioamanga@gmail.com <studioamanga@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-09 08:36:54 +0000

    Version PHP (sprint 1, complet)

---
+++ trunk/src_php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+		<title>Co-voiturage</title>
+	</head>
+	<body>
+		<header>
+			<h1>Co-voiturage</h1>
+		</header>
+		<nav>
+			<ul>
+				<li><a href="add.php">Ajouter un trajet</a></li>
+				<li><a href="list.php">Consulter les trajets</a></li>
+			</ul>
+		</nav>
+	</body>
+</html>
