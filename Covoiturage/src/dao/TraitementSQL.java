042a655bf409d8f81b8ccb6d7cb650bbaa90dbc6
wanghaoalain@gmail.com <wanghaoalain@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-14 14:35:40 +0000

    Enlever les imports inutils.

--- Covoiturage/src/dao/TraitementSQL.java
+++ Covoiturage/src/dao/TraitementSQL.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
