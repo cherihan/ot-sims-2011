3725839e478dec741e63dcc646c753399ddf046f
simon.minotto@gmail.com <simon.minotto@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-14 13:58:45 +0000

    Modification du modele : ajout de criterion, modif de user, ajout de comment...

--- Base de donnees/v2/sims.v2.create.sql
+++ Base de donnees/v2/sims.v2.create.sql
@@ -1,21 +1,82 @@
+-- TODO constraint for criterion
+
+
 -- sims SQL V2, Sprint 2
 
 -- From Simon Minotto
 
-CREATE TABLE user_usr (
+
+
+CREATE TABLE IF NOT EXISTS _criterion_type_ctt (
+	ctt_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	ctt_label VARCHAR(100) NOT NULL,
+								  
+	INDEX(ctt_label)
+
+)type=InnoDb;
+
+
+CREATE TABLE IF NOT EXISTS _criterion_crt (
+	crt_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	crt_type INT(11) NOT NULL,
+	crt_label VARCHAR(100) NOT NULL,
+	crt_root_criterion INT(11) NULL DEFAULT NULL COMMENT 'Father, used for arborescence criterions. NULL for root element',
+	crt_order INT(11) NOT NULL DEFAULT 1,
+							 
+	INDEX(crt_type),
+	INDEX(crt_root_criterion),
+	INDEX(crt_label),
+	INDEX(crt_order)
+
+)type=InnoDb;
+
+
+-- Table de cache permettant de faire des recherches arborescentes plus facilement
+CREATE TABLE IF NOT EXISTS crt_crt (
+	crt_container INT(11) NOT NULL,
+	crt_contained INT(11) NOT NULL,
+					  
+	PRIMARY KEY (crt_container, crt_contained),
+	INDEX(crt_container),
+	INDEX(crt_contained)
+	
+) type=InnoDb;
+
+
+
+CREATE TABLE IF NOT EXISTS user_usr (
 	usr_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	usr_firstname VARCHAR(100) NOT NULL,
 	usr_lastname VARCHAR(100) NOT NULL,
 	usr_email VARCHAR(100) NOT NULL COMMENT 'email is login',
 	usr_password VARCHAR(100) NOT NULL COMMENT 'password is md5(concat(usr_id,password_clear))',
 	usr_current_position INT(11) NULL DEFAULT NULL,
+	usr_genre ENUM('male','female') NOT NULL,
+	usr_birthdate BIGINT(20) NOT NULL,
+	usr_description TEXT NOT NULL,
+	usr_mobilphone VARCHAR(100) NOT NULL,
+--	usr_reputation INT(11) NOT NULL,
+	usr_note INT(2) NULL DEFAULT NULL COMMENT 'Member review average of this member, null for none', 
+	
+					   
 					   
 	UNIQUE(usr_email),
 	INDEX(usr_current_position)
 ) type=InnoDb;
 
 
-CREATE TABLE position_pos (
+CREATE TABLE IF NOT EXISTS usr_crt (
+	usr_id INT(11) NOT NULL,
+	crt_id INT(11) NOT NULL,
+					  
+	PRIMARY KEY(usr_id, crt_id),
+	INDEX(usr_id),
+	INDEX(crt_id)
+
+)type=InnoDb;
+
+
+CREATE TABLE IF NOT EXISTS position_pos (
 	pos_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	pos_address VARCHAR(255) NOT NULL,
 	pos_latitude FLOAT(10,6) NOT NULL,
@@ -26,7 +87,7 @@ CREATE TABLE position_pos (
 	INDEX(pos_address)
 )type=InnoDb;
 
-CREATE TABLE car_car (
+CREATE TABLE IF NOT EXISTS car_car (
 	car_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	car_name VARCHAR(100) NOT NULL,
 	car_seat INT(2) NULL DEFAULT NULL,
@@ -36,7 +97,7 @@ CREATE TABLE car_car (
 
 )type=InnoDb;
 
-CREATE TABLE _route_type_rtp (
+CREATE TABLE IF NOT EXISTS _route_type_rtp (
 	rtp_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	rtp_label VARCHAR(100) NOT NULL,
 							  
@@ -44,7 +105,7 @@ CREATE TABLE _route_type_rtp (
 )type=InnoDb;
 
 
-CREATE TABLE route_rte (
+CREATE TABLE IF NOT EXISTS route_rte (
 	rte_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	rte_type INT(11) NOT NULL COMMENT 'Type of traject : I want one or I propose one',
 	rte_pos_begin INT(11) NOT NULL,
@@ -69,7 +130,7 @@ CREATE TABLE route_rte (
 )type=InnoDb;
 
 
-CREATE TABLE user_fav_pos_ufp (
+CREATE TABLE IF NOT EXISTS user_fav_pos_ufp (
 	ufp_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	ufp_user INT(11) NOT NULL,
 	ufp_position INT(11) NOT NULL,
@@ -79,7 +140,7 @@ CREATE TABLE user_fav_pos_ufp (
 	
 )type=InnoDb;
 
-CREATE TABLE passager_psg (
+CREATE TABLE IF NOT EXISTS passager_psg (
 	psg_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	psg_route INT(11) NOT NULL,
 	psg_user INT(11) NOT NULL,
@@ -92,7 +153,7 @@ CREATE TABLE passager_psg (
 
 
 
-CREATE TABLE googlecache_gch (
+CREATE TABLE IF NOT EXISTS googlecache_gch (
 	gch_id INT(11) PRIMARY KEY AUTO_INCREMENT,
 	gch_address VARCHAR(255) NOT NULL,
 	gch_latitude FLOAT(10,6) NULL DEFAULT NULL,
@@ -108,14 +169,29 @@ CREATE TABLE googlecache_gch (
 
 
 
+CREATE TABLE IF NOT EXISTS comment_cmn (
+	cmn_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	cmn_user_from INT(11) NOT NULL,
+	cmn_user_to INT(11) NOT NULL,
+	cmn_comment_text TEXT NULL DEFAULT NULL COMMENT 'Textual comment about the owner',
+	cmn_comment_note INT(2) NULL DEFAULT NULL COMMENT 'A value between 1-5 to evaluate the conductor',
+
+	INDEX(cmn_user_from),
+	INDEX(cmn_user_to),
+	INDEX(cmn_comment_note)
+						  
+)type=innoDb;
+
+
+
+
+
 
 ALTER TABLE user_usr
 	ADD CONSTRAINT usr_ufp_constraint FOREIGN KEY (usr_current_position) REFERENCES user_fav_pos_ufp (ufp_id);
 	
 ALTER TABLE car_car
 	ADD CONSTRAINT car_owner_constraint FOREIGN KEY (car_owner) REFERENCES user_usr (usr_id);
-	
-	
 	
 ALTER TABLE route_rte
 	ADD CONSTRAINT route_rte_type_contraint FOREIGN KEY (rte_type) REFERENCES _route_type_rtp(rtp_id);
@@ -131,19 +207,41 @@ ALTER TABLE route_rte
 		
 ALTER TABLE route_rte
 	ADD CONSTRAINT route_car_constraint FOREIGN KEY (rte_car) REFERENCES car_car (car_id);
-		
-	
 
 ALTER TABLE user_fav_pos_ufp
 	ADD CONSTRAINT user_fav_user_constraint FOREIGN KEY (ufp_user) REFERENCES user_usr (usr_id);
 	
 ALTER TABLE user_fav_pos_ufp
-	ADD CONSTRAINT user_fav_position_constraint FOREIGN KEY (ufp_position) REFERENCES position_pos(pos_id);
-	
+	ADD CONSTRAINT user_fav_position_constraint FOREIGN KEY (ufp_position) REFERENCES position_pos(pos_id);	
 	
 ALTER TABLE passager_psg
 	ADD CONSTRAINT passager_route_constraint FOREIGN KEY (psg_route) REFERENCES route_rte (rte_id);
 	
 ALTER TABLE passager_psg
 	ADD CONSTRAINT passager_user_constraint FOREIGN KEY (psg_user) REFERENCES passager_psg (psg_id);
+	
+ALTER TABLE comment_cmn
+	ADD CONSTRAINT comment_user_from_constraint FOREIGN KEY (cmn_user_from) REFERENCES user_usr (usr_id);
+	
+ALTER TABLE comment_cmn
+	ADD CONSTRAINT comment_user_to_constraint FOREIGN KEY (cmn_user_to) REFERENCES user_usr (usr_id);
+	
+ALTER TABLE _criterion_crt
+	ADD CONSTRAINT criterion_type_constraint FOREIGN KEY (crt_type) REFERENCES _criterion_type_ctt (ctt_id);
+	
+ALTER TABLE _criterion_crt
+	ADD CONSTRAINT criterion_root_constraint FOREIGN KEY (crt_root_criterion) REFERENCES _criterion_crt (crt_id);
+
+ALTER TABLE crt_crt
+	ADD CONSTRAINT crt_crt_container_constraint FOREIGN KEY (crt_container) REFERENCES _criterion_crt (crt_id);
+
+ALTER TABLE crt_crt
+	ADD CONSTRAINT crt_crt_contained_constraint FOREIGN KEY (crt_contained) REFERENCES _criterion_crt (crt_id);
+
+ALTER TABLE usr_crt
+	ADD CONSTRAINT usr_crt_user_constraint FOREIGN KEY (usr_id) REFERENCES user_usr (usr_id);
+
+ALTER TABLE usr_crt
+	ADD CONSTRAINT usr_crt_criterion_constraint FOREIGN KEY (crt_id) REFERENCES _criterion_crt (crt_id);
+	
 	
