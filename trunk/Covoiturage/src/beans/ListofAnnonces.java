f4244def839afdbe69b42466ab8f59c58331e5a6
wanghaoalain@gmail.com <wanghaoalain@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-14 14:35:48 +0000

    Enlever les imports inutils.

--- trunk/Covoiturage/src/beans/ListofAnnonces.java
+++ trunk/Covoiturage/src/beans/ListofAnnonces.java
@@ -1,10 +1,5 @@
 package beans;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.annotation.PostConstruct;
