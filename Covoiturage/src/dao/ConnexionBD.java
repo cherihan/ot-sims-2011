21c3eaf0abc211c0b626c7d6bbc7edf4fcd9722c
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-15 13:23:58 +0000

    enlevement des exception afin de récuppérer les messages d'exception ailleur

--- Covoiturage/src/dao/ConnexionBD.java
+++ Covoiturage/src/dao/ConnexionBD.java
@@ -9,62 +9,43 @@ public class ConnexionBD {
     public ResultSet           re=null;
     public ResultSetMetaData   metaBase;
     
-    public ConnexionBD(String url, String nomDriver) {
-        try {
+    public ConnexionBD(String url, String nomDriver) throws SQLException, ClassNotFoundException {
             Class.forName(nomDriver);
             con = DriverManager.getConnection(url);
             System.out.println("Overture de la connection");
             sta = con.createStatement();
-        }
-        catch (ClassNotFoundException ex) {
-            System.err.println("Ne peut pas trouver les classes du conducteur de la base de données.");
-            System.err.println(ex);
-        }
-        catch (SQLException ex) {
-            System.err.println("pas de connection au base de de donnee.");
-            System.err.println(ex);
-        }
+        
+        
      }
     
-    public ResultSet search(String query){
-    	try { 
+    
+    public ResultSet search(String query) throws SQLException{
     	re=sta.executeQuery(query);
     	System.out.println("Element trouvee ");
-    		}
-    	catch (SQLException ex) {
-            System.err.println(ex);
-         }
+    		
     	return re;
     	}
     
-    public void insert(String query){
-    	try { 
+    public void insert(String query) throws SQLException{
     	sta.executeUpdate(query);
     	System.out.println("Element ajouter a la base");
-    		}
-    	catch (SQLException ex) {
-            System.err.println(ex);
-    	}      
-    }   
-    public void update(String query){
-    	try { 
+    	    
+    }  
+    
+    
+    public void update(String query) throws SQLException{
+    	
     	sta.executeUpdate(query);
     	System.out.println("Element Modifier");
-    	}
-    	catch (SQLException ex){
-        System.err.println(ex);
-    	}  	
+    	
     } 
     
     
-    public void delete(String query){
-    	try { 
+    public void delete(String query) throws SQLException{
+    	 
     	sta.executeUpdate(query);
     	System.out.println("Element suprimer");    	
-    	}
-    	catch (SQLException ex) {
-         System.err.println(ex);
-    	}  	
+    	 	
     }
     
     
