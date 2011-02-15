a42fff154f3dc6070137047ae0aa8180981c4b28
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-15 20:45:35 +0000

    ajout de quelque messages

--- Covoiturage/src/utilities/Constantes.java
+++ Covoiturage/src/utilities/Constantes.java
@@ -8,5 +8,7 @@ public class Constantes {
 	 public static String CLASS_DB_NOT_FOUND = "Ne peut pas trouver les classes du conducteur de la base de données";
 	 public static String PASSWORD_NOT_IDENTIQUE_OR_NULL = "Mot de passe n'est pas identique ou nulle";
 	 public static String DATAS_NOT_FILL_IN = "Vous n'avez pas remplit tous les champs";
-	 public static String PASSWORD_OR_USER_NOT_GOOD = "Mot de passe ou Email n'est pas bon";
+	 public static String EMAIL_FORM_NOT_CORRECT = "la forme de l'adresse email que vous avez saisi n'est pas correcte";
+	 public static String DATA_FORM_NOT_CORRECT = "la forme de l'email que vous avez saisi n'est pas bonne";
+	 public static String PASSWORD_OR_USER_NOT_CORRECT = "Mot de passe ou Email n'est pas correcte";
 }
