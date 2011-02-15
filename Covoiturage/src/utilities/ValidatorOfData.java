7496e21e4b362d6a4b6316adb631eb24dbea7bd4
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-15 20:46:19 +0000

    ajout d'un class qui va contenir des fonction pour la validation des données

---
+++ Covoiturage/src/utilities/ValidatorOfData.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class ValidatorOfData {
+	
+	public static Boolean validateData(String data){
+			
+		//TODO
+		
+		return true;
+	}
+	
+	public static Boolean validateEMail(String mail){
+						
+		ArrayList<String> deviseMail = new ArrayList<String>();
+			
+			StringTokenizer st = new StringTokenizer(mail,".@");
+			
+			while(st.hasMoreElements()){
+				deviseMail.add(st.nextElement().toString());
+			}
+			
+			if(deviseMail.size() != 4) return false;
+			
+			String compareMail;
+			
+			compareMail = deviseMail.get(0)+ "." + deviseMail.get(1) + "@" + deviseMail.get(2) + "." + deviseMail.get(3);
+			
+			if(!compareMail.equals(mail))return false;
+		
+		return true;
+	}
+	
+	
+
+}
