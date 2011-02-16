4062be1b1c372821da5bba6efa668b6e44589227
simon.minotto@gmail.com <simon.minotto@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-16 09:52:07 +0000

    generation du code du model

--- trunk/Covoiturage/src/model/Position.java
+++ trunk/Covoiturage/src/model/Position.java
@@ -9,10 +9,71 @@ public class Position {
 
 
 
+	/**
+	 * @param id
+	 * @param address
+	 * @param latitude
+	 * @param longitude
+	 */
+	public Position(int id, String address, double latitude, double longitude) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 	protected int id;
 	protected String address;
-	protected float latitude;
-	protected float longitude;
+	protected double latitude;
+	protected double longitude;
 
 
 }
