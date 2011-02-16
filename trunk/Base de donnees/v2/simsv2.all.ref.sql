ed4330d43bcb9a436c0ae7f5dd0be7575ab2b32a
samuel43@laposte.net <samuel43@laposte.net@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-16 10:36:03 +0000

    Create a SQL document that references all the others

---
+++ trunk/Base de donnees/v2/simsv2.all.ref.sql
@@ -0,0 +1,13 @@
+\. ./sims.v2.drop.sql
+
+delimiter //
+
+\. ./procedures/sims.v2.procedure.criterion.sql
+\. ./procedures/sims.v2.procedure.position.sql
+\. ./procedures/sims.v2.procedure.route.sql
+\. ./procedures/sims.v2.procedure.user.sql
+
+delimiter ;
+
+\. ./sims.v2.create.sql
+\. ./sims.v2.insert.sql
