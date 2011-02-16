5c2ce7f05c7f6ea484c56c17ecac4134cc7c1350
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-16 15:43:21 +0000

    Clas qui sert pour la récuperation des bean qui sont stocket dans la session

---
+++ Covoiturage/src/utilities/FacesUtil.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import javax.faces.context.FacesContext;
+
+public class FacesUtil {
+
+    /**
+     * Cette fonction sert pour recuperer les beans stocket dans la session.
+     * le parametre name et de la forme "#{bean.attribut}" 
+     * @param name
+     * @return "un objet selon le type de l'attribut de bean" 
+     */
+    public static Object getParameter(String name) {
+    	FacesContext fc = FacesContext.getCurrentInstance();   	
+    	   	
+    	return (Object) fc.getApplication().createValueBinding(name).getValue(fc);
+    }
+    
+     
+}
