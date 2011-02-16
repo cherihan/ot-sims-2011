689e575240bd198fe2bde2a8a6a3158e72baf19f
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-16 15:56:28 +0000

    ajout d'un exemple pour savoir comment utiliser la fonction getParametr()

--- trunk/Covoiturage/src/utilities/FacesUtil.java
+++ trunk/Covoiturage/src/utilities/FacesUtil.java
@@ -9,6 +9,11 @@ public class FacesUtil {
      * le parametre name et de la forme "#{bean.attribut}" 
      * @param name
      * @return "un objet selon le type de l'attribut de bean" 
+     * 
+     * Exemple :
+     *  
+     * User utilisateur = (User) FacesUtil.getParameter("#{beansUser.user}");
+     * 
      */
     public static Object getParameter(String name) {
     	FacesContext fc = FacesContext.getCurrentInstance();   	
