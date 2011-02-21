2d008632535dada5fb75037703c0d59625fca038
simon.minotto@gmail.com <simon.minotto@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-21 08:57:46 +0000

    Ajout DaoCar, modif du model pour getter d'objets

---
+++ trunk/Covoiturage/src/dao/DaoCar.java
@@ -0,0 +1,84 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+
+import utilities.Constantes;
+import model.Car;
+import model.User;
+
+public class DaoCar {
+
+	public static ConnexionBD con;
+
+	/**
+	 * 
+	 * @param email
+	 * @param passWord
+	 * @return User
+	 * @throws Exception
+	 */
+	public static Car createCar(String name, Integer seat, Integer owner)
+			throws Exception {
+
+		con = null;
+		String messageErr = null;
+		ResultSet res;
+		Car car = null;
+
+		try {
+
+			con = new ConnexionBD(ConnexionBD.url, ConnexionBD.nomDriver);
+
+			String query;
+
+			query = "call car_create('" + ConnexionBD.escape(name) + "', "
+					+ seat + ", '" + owner + "')";
+
+			res = con.execute(query);
+			if (res.first())
+				car = new Car(res);
+		} catch (ClassNotFoundException ex) {
+			messageErr = Constantes.CLASS_DB_NOT_FOUND;
+			System.err.println(messageErr + " : " + ex);
+			throw new Exception(messageErr);
+		} catch (SQLException ex) {
+			messageErr = Constantes.PROBLEME_CONNECTION_DB;
+			System.err.println(messageErr + " : " + ex);
+			throw new Exception(messageErr);
+		}
+
+		if (con != null)
+			con.close();
+
+		return car;
+
+	}
+
+
+	public static Car getCar(int car_id) {
+		Car car = null;
+		try {
+			con = new ConnexionBD(ConnexionBD.url, ConnexionBD.nomDriver);
+
+			String query = "SELECT * FROM car_car WHERE car_id= " + car_id
+					+ "";
+
+			ResultSet curseur = con.execute(query);
+			if (curseur.first()) {
+				car = new Car(curseur);
+				return car;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static Car getCar(Car car) {
+		return DaoCar.getCar(car.getId());
+	}
+}
