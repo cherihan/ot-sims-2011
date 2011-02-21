a8f0f64d7df6cd5437315d96d220bed7d9e2e9dd
samuel43@laposte.net <samuel43@laposte.net@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-21 02:35:23 +0000

    Put some looooooove into python man

--- trunk/src_python/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists departures;
-create table departures (
-  id integer primary key autoincrement,
-  starts_from string not null,
-  goes_to string not null,
-  ts timestamp not null
-);
-
