79eaedb43fb4c144175cad7cfb45600ae59fdd7d
simon.minotto@gmail.com <simon.minotto@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-21 14:40:01 +0000

    Update des procedures

--- trunk/Base de donnees/v2/simsv2.all.ref.sql
+++ trunk/Base de donnees/v2/simsv2.all.ref.sql
@@ -6,8 +6,10 @@ delimiter //
 \. ./procedures/sims.v2.procedure.position.sql
 \. ./procedures/sims.v2.procedure.route.sql
 \. ./procedures/sims.v2.procedure.user.sql
+\. ./procedures/sims.v2.procedure.comment.sql
 
 delimiter ;
+
 
 \. ./sims.v2.create.sql
 \. ./sims.v2.insert.sql
