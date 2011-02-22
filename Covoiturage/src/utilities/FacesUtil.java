81cc4ab23853082d643a4e4e3400baf49465b6cf
HyacinthMao@gmail.com <HyacinthMao@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-22 10:11:16 +0000

    removed deprecated methods

--- Covoiturage/src/utilities/FacesUtil.java
+++ Covoiturage/src/utilities/FacesUtil.java
@@ -6,38 +6,45 @@ import model.User;
 
 public class FacesUtil {
 
-    /**
-     * Cette fonction sert pour recuperer les beans stocket dans la session.
-     * le parametre name et de la forme "#{bean.attribut}" 
-     * @param name
-     * @return "un objet selon le type de l'attribut de bean" 
-     * 
-     * Exemple :
-     *  
-     * User utilisateur = (User) FacesUtil.getParameter("#{beansUser.user}");
-     * 
-     */
-	@SuppressWarnings("deprecation")
+	/**
+	 * Cette fonction sert pour recuperer les beans stocket dans la session. le
+	 * parametre name et de la forme "#{bean.attribut}"
+	 * 
+	 * @param name
+	 * @return "un objet selon le type de l'attribut de bean"
+	 * 
+	 *         Exemple :
+	 * 
+	 *         User utilisateur = (User)
+	 *         FacesUtil.getParameter("#{beansUser.user}");
+	 * 
+	 */
+
 	public static Object getParameter(String name) {
-    	FacesContext fc = FacesContext.getCurrentInstance();   	
-    	   	
-    	return (Object) fc.getApplication().createValueBinding(name).getValue(fc);
-    }
-	
-	
-	@SuppressWarnings("deprecation")
+		FacesContext fc = FacesContext.getCurrentInstance();
+
+		return (Object) fc.getApplication().getExpressionFactory()
+				.createValueExpression(fc.getELContext(), name, Object.class)
+				.getValue(fc.getELContext());
+	}
+
 	public static User getUser() {
-    	FacesContext fc = FacesContext.getCurrentInstance();   	
-    	   	
-    	return (User) fc.getApplication().createValueBinding("#{beansUser.user}").getValue(fc);
-    }
-	
-	@SuppressWarnings("deprecation")
+		FacesContext fc = FacesContext.getCurrentInstance();
+
+		return (User) fc
+				.getApplication()
+				.getExpressionFactory()
+				.createValueExpression(fc.getELContext(), "#{beansUser.user}",
+						User.class).getValue(fc.getELContext());
+	}
+
 	public static void setUser(User usr) {
-    	FacesContext fc = FacesContext.getCurrentInstance();   	
-    	   	
-    	fc.getApplication().createValueBinding("#{beansUser.user}").setValue(fc, usr);
-    }
-    
-     
+		FacesContext fc = FacesContext.getCurrentInstance();
+
+		fc.getApplication()
+				.getExpressionFactory()
+				.createValueExpression(fc.getELContext(), "#{beansUser.user}",
+						User.class).setValue(fc.getELContext(), usr);
+	}
+
 }
