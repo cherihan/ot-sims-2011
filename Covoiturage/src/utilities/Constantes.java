19f467795e16471b022897d4a6f7e661ede5d51a
studioamanga@gmail.com <studioamanga@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-02-23 08:50:21 +0000

    Uniformisation syntaxique

--- Covoiturage/src/utilities/Constantes.java
+++ Covoiturage/src/utilities/Constantes.java
@@ -1,18 +1,17 @@
 package utilities;
 
 public class Constantes {
-	
-	 public static String USER_ALREADY_SAVED = "Cet utilisateur existe déja";
-	 public static String PROBLEME_CONNECTION_DB = "Pas de connection à la base de donn�es";
-	 public static String OTHER_PROBLEME_IN_CONNECTION_DB = "Connection échouée";
-	 public static String CLASS_DB_NOT_FOUND = "Ne peut pas trouver les classes du conducteur dans la base de données";
-	 public static String PASSWORD_NOT_IDENTIQUE_OR_NULL = "La confirmation du mot de passe est incorrecte";
-	 public static String DATAS_NOT_FILL_IN = "Vous n'avez pas rempli tous les champs";
-	 public static String EMAIL_FORM_NOT_CORRECT = "La forme de l'adresse email que vous avez saisi n'est pas correcte";
-	 public static String DATA_FORM_NOT_CORRECT = "La forme de données que vous avez saisi n'est pas correcte";
-	 public static String MOBILE_NUMBER_FORM_NOT_CORRECT = "La forme de numero de telephone que vous avez saisi n'est pas correcte";
-	 public static String PASSWORD_OR_USER_NOT_CORRECT = "Mot de passe ou Email incorrecte";
-	 public static String PASSWORD_FORM_NOT_CORRECT = "Mot de passe ne respecte pas la forme validée";
-	 public static String UNEXPECTED_ERROR = "Une erreur inattendue est survenue ...";
-	 public static String ROUTE_ALREADY_PASSSAGER = "Déjà passager du trajet";
+	public static String USER_ALREADY_SAVED = "Cet utilisateur est déjà enregistré";
+	public static String PROBLEME_CONNECTION_DB = "Erreur de connexion à la base de données";
+	public static String OTHER_PROBLEME_IN_CONNECTION_DB = "Échec de connexion à la base de données";
+	public static String CLASS_DB_NOT_FOUND = "Impossible de trouver la classe conducteur dans la base de données";
+	public static String PASSWORD_NOT_IDENTIQUE_OR_NULL = "La confirmation du mot de passe est incorrecte";
+	public static String DATAS_NOT_FILL_IN = "Veuillez remplir tous les champs demandés";
+	public static String EMAIL_FORM_NOT_CORRECT = "La forme de l'adresse e-mail que vous avez saisie n'est pas correcte";
+	public static String DATA_FORM_NOT_CORRECT = "Le format des données est incorrect";
+	public static String MOBILE_NUMBER_FORM_NOT_CORRECT = "Le format du numéro de téléphone est incorrect";
+	public static String PASSWORD_OR_USER_NOT_CORRECT = "Mot de passe ou e-mail incorrect";
+	public static String PASSWORD_FORM_NOT_CORRECT = "Le mot de passe ne respecte pas la forme validée";
+	public static String UNEXPECTED_ERROR = "Une erreur inattendue est survenue :-(";
+	public static String ROUTE_ALREADY_PASSSAGER = "Déjà passager du trajet";
 }
