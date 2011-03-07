f6c7c571dbd204dcb6ca79ddbf1626dc1047afb2
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-03-07 10:16:24 +0000

    beans util pour passer les paramêtre d'une page à l'autre

---
+++ trunk/Covoiturage/src/beans/ParamsBean.java
@@ -0,0 +1,28 @@
+package beans;
+
+public class ParamsBean {
+    
+	private String parameter;
+    
+    public ParamsBean() {
+       
+    }
+
+	/**
+	 * @return the parameter
+	 */
+	public String getParameter() {
+		return parameter;
+	}
+
+	/**
+	 * @param parameter the parameter to set
+	 */
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
+	}
+    
+    
+    
+    
+}
