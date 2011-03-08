f42a3f3cb90e1a67e519690cb7cae496c11805cd
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-03-08 14:49:03 +0000

    suppresion d'un println de test

--- trunk/Covoiturage/src/beans/BeansRoute.java
+++ trunk/Covoiturage/src/beans/BeansRoute.java
@@ -10,7 +10,6 @@ import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
-
 import dao.DaoRoute;
 import dao.DaoPosition;
 import dao.DaoUser_fav_position;
@@ -34,28 +33,26 @@ public class BeansRoute {
 	protected String pos_depart_other = null;
 	protected String pos_depart_coords_lat = null;
 	protected String pos_depart_coords_lng = null;
-	
-	
+
 	protected String pos_arrive = null;
 	protected String pos_arrive_other = null;
 	protected String pos_arrive_coords_lat = null;
 	protected String pos_arrive_coords_lng = null;
-	
-	
+
 	protected Integer route_type = null;
 	protected Integer minutes_to_depart = null;
 	protected Integer seat_number = null;
 	protected Integer time_delta = null;
 	private Integer distance_radius = null;
-	
+
 	protected Boolean is_my_route = false;
 	protected Boolean is_created_route = false;
 	protected ArrayList<User_fav_position> user_fav_pos = null;
-	
+
 	private List<Route> route_list = new ArrayList<Route>();
 	protected String messageErr;
-		
-    private String parameter;	
+
+	private String parameter;
 
 	/**
 	 * @return the parameter
@@ -65,12 +62,12 @@ public class BeansRoute {
 	}
 
 	/**
-	 * @param parameter the parameter to set
+	 * @param parameter
+	 *            the parameter to set
 	 */
 	public void setParameter(String parameter) {
 		this.parameter = parameter;
 	}
-	
 
 	public Integer getSeat_number() {
 		return seat_number;
@@ -116,15 +113,12 @@ public class BeansRoute {
 	 * @return
 	 */
 	public Route getRoute() {
-		
-System.out.println("parameter: ****** " + parameter + "******" );		
-		if(parameter != null && parameter.length()!=0) {
+		if (parameter != null && parameter.length() != 0) {
 			int rte_id = Integer.parseInt(parameter);
-			route = DaoRoute.getRoute(rte_id);			
+			route = DaoRoute.getRoute(rte_id);
 			parameter = null;
-			System.out.println("Test: ****** Test Othman ******" );	
 		}
-		
+
 		return route;
 	}
 
@@ -153,22 +147,20 @@ System.out.println("parameter: ****** " + parameter + "******" );
 	 * Create new route
 	 */
 	public String createRoute() {
-		//Route createdRoute = null;
+		// Route createdRoute = null;
 		route_type = Route_type.PROVIDE_CAR;
-		if (minutes_to_depart == null 
-				|| pos_depart == null
-				|| pos_arrive == null 
-				|| route_type == null) {
+		if (minutes_to_depart == null || pos_depart == null
+				|| pos_arrive == null || route_type == null) {
 			messageErr = Constantes.DATAS_NOT_FILL_IN;
 			return "actuel";
 		}
-		
-		if (route_type == 2){
-			if (seat_number <= 0){
+
+		if (route_type == 2) {
+			if (seat_number <= 0) {
 				messageErr = "Seat number must be greater than 0";
 				return "actuel";
 			}
-		}else{
+		} else {
 			seat_number = null;
 		}
 
@@ -177,25 +169,24 @@ System.out.println("parameter: ****** " + parameter + "******" );
 		Date full_date_depart = c.getTime();
 
 		User currentUser = FacesUtil.getUser();
-		
+
 		try {
-			
+
 			Position posBegin = null;
 			Position posEnd = null;
-			
+
 			posBegin = this.getSelectedPositionBegin();
 			posEnd = this.getSelectedPositionEnd();
-			
-			
-			
-			route = DaoRoute.createRoute(Route_type.PROVIDE_CAR, posBegin.getId(), posEnd.getId(),
-					full_date_depart, null, null, currentUser.getId(), seat_number, null);
-			if (route != null){
+
+			route = DaoRoute.createRoute(Route_type.PROVIDE_CAR,
+					posBegin.getId(), posEnd.getId(), full_date_depart, null,
+					null, currentUser.getId(), seat_number, null);
+			if (route != null) {
 				is_created_route = true;
 				route.setPassagers(new Hashtable<Integer, Passager>());
 				System.out.println("new route created");
 				return "show";
-			}else{
+			} else {
 				this.messageErr = Constantes.UNEXPECTED_ERROR;
 				return "actuel";
 			}
@@ -205,58 +196,62 @@ System.out.println("parameter: ****** " + parameter + "******" );
 			return "actuel";
 		}
 	}
-	
+
 	protected Position getSelectedPositionBegin() throws Exception {
-		Position posBegin=null;
-		if(pos_depart.equals("here")) {
+		Position posBegin = null;
+		if (pos_depart.equals("here")) {
 			Double lat = Double.valueOf(pos_depart_coords_lat);
 			Double lng = Double.valueOf(pos_depart_coords_lng);
-			if(lat == 0)
+			if (lat == 0)
 				throw new Exception(Constantes.DATAS_NOT_FILL_IN);
-			if(lng == 0)
+			if (lng == 0)
 				throw new Exception(Constantes.DATAS_NOT_FILL_IN);
 			Hashtable<String, Double> coords = new Hashtable<String, Double>();
 			coords.put("latitude", lat);
 			coords.put("longitude", lng);
 			String address = GoogleGeoApiCached.getNearAddressFromCoord(coords);
 			posBegin = DaoPosition.createPosition(address, lat, lng);
-		}else if(pos_depart.equals("other")) {
+		} else if (pos_depart.equals("other")) {
 			Hashtable<String, Double> coords;
 			coords = GoogleGeoApiCached.getCoordOfAddress(pos_depart_other);
-			posBegin = DaoPosition.createPosition(pos_depart_other, coords.get("latitude"), coords.get("longitude"));
-		}else{
+			posBegin = DaoPosition.createPosition(pos_depart_other,
+					coords.get("latitude"), coords.get("longitude"));
+		} else {
 			Integer ufp_id = Integer.valueOf(pos_depart);
-			User_fav_position ufp = DaoUser_fav_position.getUser_fav_position(ufp_id);
+			User_fav_position ufp = DaoUser_fav_position
+					.getUser_fav_position(ufp_id);
 			posBegin = ufp.getPositionObj();
 		}
 		return posBegin;
 	}
-	
+
 	protected Position getSelectedPositionEnd() throws Exception {
-		Position posEnd=null;
-		
-		if(pos_arrive.equals("here")) {
+		Position posEnd = null;
+
+		if (pos_arrive.equals("here")) {
 			Double lat = Double.valueOf(pos_arrive_coords_lat);
 			Double lng = Double.valueOf(pos_arrive_coords_lng);
-			if(lat == 0)
+			if (lat == 0)
 				throw new Exception(Constantes.DATAS_NOT_FILL_IN);
-			if(lng == 0)
+			if (lng == 0)
 				throw new Exception(Constantes.DATAS_NOT_FILL_IN);
 			Hashtable<String, Double> coords = new Hashtable<String, Double>();
 			coords.put("latitude", lat);
 			coords.put("longitude", lng);
 			String address = GoogleGeoApiCached.getNearAddressFromCoord(coords);
 			posEnd = DaoPosition.createPosition(address, lat, lng);
-		}else if(pos_arrive.equals("other")) {
+		} else if (pos_arrive.equals("other")) {
 			Hashtable<String, Double> coords;
 			coords = GoogleGeoApiCached.getCoordOfAddress(pos_arrive_other);
-			posEnd = DaoPosition.createPosition(pos_arrive_other, coords.get("latitude"), coords.get("longitude"));
-		}else{
+			posEnd = DaoPosition.createPosition(pos_arrive_other,
+					coords.get("latitude"), coords.get("longitude"));
+		} else {
 			Integer ufp_id = Integer.valueOf(pos_arrive);
-			User_fav_position ufp = DaoUser_fav_position.getUser_fav_position(ufp_id);
+			User_fav_position ufp = DaoUser_fav_position
+					.getUser_fav_position(ufp_id);
 			posEnd = ufp.getPositionObj();
 		}
-		
+
 		return posEnd;
 	}
 
@@ -272,26 +267,28 @@ System.out.println("parameter: ****** " + parameter + "******" );
 			messageErr = Constantes.DATAS_NOT_FILL_IN;
 			return "actuel";
 		}
-		
+
 		/*
-		Position from = DaoPosition.getPositionByAddress(pos_depart);
-		Position to = DaoPosition.getPositionByAddress(pos_arrive);
-		*/
-		
+		 * Position from = DaoPosition.getPositionByAddress(pos_depart);
+		 * Position to = DaoPosition.getPositionByAddress(pos_arrive);
+		 */
+
 		Position posBegin = null;
 		Position posEnd = null;
-		
+
 		posBegin = this.getSelectedPositionBegin();
 		posEnd = this.getSelectedPositionEnd();
-		
 
 		Date date_departure_begin = new Date();
-		date_departure_begin.setTime(date_departure_begin.getTime() + (minutes_to_depart - time_delta)*60*1000);
-		
-		Date date_departure_end = new Date();
-		date_departure_end.setTime(date_departure_end.getTime() + (minutes_to_depart + time_delta)*60*1000);
+		date_departure_begin.setTime(date_departure_begin.getTime()
+				+ (minutes_to_depart - time_delta) * 60 * 1000);
 
-		Hashtable<Integer, Route> table = DaoRoute.route_search(posBegin, posEnd, date_departure_begin, date_departure_end,
+		Date date_departure_end = new Date();
+		date_departure_end.setTime(date_departure_end.getTime()
+				+ (minutes_to_depart + time_delta) * 60 * 1000);
+
+		Hashtable<Integer, Route> table = DaoRoute.route_search(posBegin,
+				posEnd, date_departure_begin, date_departure_end,
 				distance_radius, Route_type.PROVIDE_CAR);
 		route_list.clear();
 		route_list.addAll(table.values());
@@ -317,7 +314,6 @@ System.out.println("parameter: ****** " + parameter + "******" );
 	public Collection<Route> getRoute_list() {
 		return route_list;
 	}
-
 
 	public String getPos_depart_other() {
 		return pos_depart_other;
@@ -370,15 +366,15 @@ System.out.println("parameter: ****** " + parameter + "******" );
 	public void setRoute_list(List<Route> route_list) {
 		this.route_list = route_list;
 	}
-	
+
 	public ArrayList<Position> getSegments() {
 		ArrayList<Position> retour = new ArrayList<Position>();
 		ArrayList<Segment> input = DaoRoute.getSegments(this.route);
-		System.out.println("Numberelementssss : "+input.size());
-		int i=0;
-		int n=input.size();
-		while(i < n) {
-			Segment seg = input.get(i); 
+		System.out.println("Numberelementssss : " + input.size());
+		int i = 0;
+		int n = input.size();
+		while (i < n) {
+			Segment seg = input.get(i);
 			retour.add(seg.getPos_beginObj());
 			retour.add(seg.getPos_endObj());
 			i++;
@@ -404,10 +400,11 @@ System.out.println("parameter: ****** " + parameter + "******" );
 
 	public ArrayList<User_fav_position> getUser_fav_pos() {
 		ArrayList<User_fav_position> retour = new ArrayList<User_fav_position>();
-		Hashtable<Integer, User_fav_position> input = DaoUser_fav_position.getFavoritePositionsOfUser(FacesUtil.getUser());
+		Hashtable<Integer, User_fav_position> input = DaoUser_fav_position
+				.getFavoritePositionsOfUser(FacesUtil.getUser());
 		Collection<User_fav_position> inpc = input.values();
 		Iterator<User_fav_position> it = inpc.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			User_fav_position ufp = it.next();
 			retour.add(ufp);
 		}
@@ -417,5 +414,5 @@ System.out.println("parameter: ****** " + parameter + "******" );
 	public void setUser_fav_pos(ArrayList<User_fav_position> user_fav_pos) {
 		this.user_fav_pos = user_fav_pos;
 	}
-	
+
 }
