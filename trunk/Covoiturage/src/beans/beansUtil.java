a1b7a92b95c25ac5333cb3d1318fd6caa8a1d773
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-03-14 17:20:57 +0000

    bean util pour le passege des paramettre

---
+++ trunk/Covoiturage/src/beans/beansUtil.java
@@ -0,0 +1,23 @@
+package beans;
+
+public class beansUtil {
+
+	private String route_id;
+
+	/**
+	 * @return the route_id
+	 */
+	public String getRoute_id() {
+		return route_id;
+	}
+
+	/**
+	 * @param route_id the route_id to set
+	 */
+	public void setRoute_id(String route_id) {
+		this.route_id = route_id;
+	}
+	
+	
+	
+}
