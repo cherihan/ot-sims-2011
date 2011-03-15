f6a99646176abbd71889f4b18f5f19037e193a36
othman.bentria@gmail.com <othman.bentria@gmail.com@67f44a2b-f73f-304d-5312-4d00d3b18584>
2011-03-15 14:54:39 +0000

    affichage du profil de n'importe quelle utilisateur

--- trunk/Covoiturage/src/beans/beansUtil.java
+++ trunk/Covoiturage/src/beans/beansUtil.java
@@ -3,6 +3,8 @@ package beans;
 public class beansUtil {
 
 	private String route_id;
+	
+	private String user_id;
 
 	/**
 	 * @return the route_id
@@ -17,6 +19,25 @@ public class beansUtil {
 	public void setRoute_id(String route_id) {
 		this.route_id = route_id;
 	}
+
+	/**
+	 * @return the user_id
+	 */
+	public String getUser_id() {
+		return user_id;
+	}
+
+	/**
+	 * @param user_id the user_id to set
+	 */
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
